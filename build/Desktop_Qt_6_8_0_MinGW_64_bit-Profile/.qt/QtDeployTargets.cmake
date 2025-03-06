652e92db0d2fc732a2ea3919d3ce19ca39d456db
Stalker72742 <romaklymenko111@gmail.com>
2025-03-06 23:48:37 +0300

    remove trash

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Profile/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_UEBuilder_FILE E:/UEBuilder/build/Desktop_Qt_6_8_0_MinGW_64_bit-Profile/UEBuilder.exe)
-set(__QT_DEPLOY_TARGET_UEBuilder_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_UEBuilder_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
